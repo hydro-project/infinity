effd3108901e229762779b9803820cf94154bb4f
Shadaj Laddad <shadajl@amazon.com>
2025-11-20 09:40:55 -0800

    Abstract tools

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use lambda_runtime::{run, service_fn, tracing, Error};
+use lambda_runtime::{Error, run, service_fn, tracing};
 
 mod event_handler;
 use event_handler::function_handler;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
