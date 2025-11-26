d6e62cd07138d1f93456fd9a37f8982e655c7c49
Shadaj Laddad <shadajl@amazon.com>
2025-11-26 12:25:26 -0800

    CDK-ify agent creation

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AgentZeroLeaderStack } from '../lib/agentzero-leader-stack';
+import { ExampleAgentStack } from '../lib/example-agent';
 
 const app = new cdk.App();
-new AgentZeroLeaderStack(app, 'AgentZeroLeaderStack', {
+new ExampleAgentStack(app, 'AgentZeroLeaderStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
---
+++ cdk/lib/tools/index.ts
@@ -0,0 +1,6 @@
+export * from './agent-zero';
+export * from './tool';
+export * from './tool-set';
+export * from './lambda-tool';
+export * from './custom-tool-set';
+export * from './mcp-tool-set';
