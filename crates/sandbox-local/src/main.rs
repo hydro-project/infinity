8918ab19cff48e63c49caaa912b016542a5826a9
Shadaj Laddad <shadajl@amazon.com>
2026-03-03 12:36:16 -0800

    Support launching RAP servers as a subprocess

--- crates/sandbox-local/src/main.rs
+++ crates/sandbox-local/src/main.rs
@@ -21,6 +21,7 @@ struct Args {
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env().add_directive("info".parse().unwrap()))
+        .with_writer(std::io::stderr)
         .init();
 
     let args = Args::parse();
